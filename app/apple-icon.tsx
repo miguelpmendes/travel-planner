23138e88fd131f13a1bdf7723092f5fb474bc291
Miguel Mendes <miguelmendes@unknown4233610ba98f.lan>
2026-03-28 20:25:13 +0000

    App icon

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -15,10 +15,16 @@ export default function AppleIcon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          fontSize: 100,
         }}
       >
-        ✈️
+        <svg
+          width="100"
+          height="100"
+          viewBox="0 0 24 24"
+          fill="white"
+        >
+          <path d="M21 16v-2l-8-5V3.5A1.5 1.5 0 0 0 11.5 2 1.5 1.5 0 0 0 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5z" />
+        </svg>
       </div>
     ),
     { ...size }
